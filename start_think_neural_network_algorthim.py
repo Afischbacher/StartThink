a7705a6e7beef658fea1b6540bd06620d9fb1fba
Afischbacher <ital-andre95@live.com>
2017-05-30 18:45:23 -0400

    setup the dumping of weights into a csv

--- start_think_neural_network_algorthim.py
+++ start_think_neural_network_algorthim.py
@@ -1,12 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-
+import csv
 # fix random seed for random based process
 np.random.seed(5392)
 
 # loading dataset
-
 dataset = np.loadtxt("data_parser/machine_learning_data.csv", delimiter=",", dtype=float)
 # split into input (X) and output (Y) variables
 X = dataset[:, 0:508]
@@ -23,3 +22,7 @@ print("Fitting Model")
 model.fit(X, Y, epochs=23197, batch_size=100)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+with open("machine_learning_weights.csv","w+", encoding="utf8") as wf:
+    writer = csv.writer(wf, delimiter=",", quotechar='"', quoting=csv.QUOTE_NONE, escapechar=" ")
+    writer.write(model.sample_weights)
